4771dc3d29e9eb2614112e76175b51556be1a9a9
Mike <mike@mikejarosch.com>
2019-02-16 18:55:34 -0600

    Initial Commit

---
+++ Stopwatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stopwatch
+//
+//  Created by Mike Jarosch on 2/16/19.
+//  Copyright © 2019 Mike Jarosch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stopwatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stopwatch
+//
+//  Created by Mike Jarosch on 2/16/19.
+//  Copyright © 2019 Mike Jarosch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
